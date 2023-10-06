0265239303fe8f6a534d7830cb860308b8c87861
Jesus H <jesus_data@hotmail.es>
2023-10-05 23:30:32 -0500

    Hooks ready

--- src/test/java/steps/AmazonSearch.java
+++ src/test/java/steps/AmazonSearch.java
@@ -3,11 +3,10 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.WebDriver;
+
 
 public class AmazonSearch{
 
-    //private WebDriver driver=Hooks.getDriver();
     @Given("The user navigate to www.amazom.com")
     public void theUserNavigateToWwwAmazomCom() {
         
--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -5,7 +5,7 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+
 
 public class Hooks {
 
